71da5ba57b8e23ba80e0270eb5b515b67cd8f820
raccoonmonk <raccoonmonk@gmail.com>
2015-07-11 15:28:59 +0300

    handle phrases

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ngram_correlation
-Program to get correlation coefficient between two words using Google Books NGrams
+Program to get correlation coefficient between two phrases using Google Books NGrams
 
 uses C++11
 
--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 
 int main()
 {
-  std::cout << "Correlation coefficient between 2 words using Google books NGrams" << std::endl;
-  std:: cout << "Enter 2 words: " << std::endl;
+  std::cout << "Correlation coefficient between 2 phrases using Google Books NGrams" << std::endl;
+  std:: cout << "Enter 2 phrases in the separate lines: " << std::endl;
   std::string word1, word2;
-  std::cin >> word1;
-  std::cin >> word2;
+  std::getline(std::cin, word1);
+  std::getline(std::cin, word2);
+  std::replace(word1.begin(), word1.end(), ' ', '+');
+  std::replace(word2.begin(), word2.end(), ' ', '+');
 
   std::cout << "Corelation coefficient = " <<
                NGramCorrelation(word1, word2).getCorrelation() <<
